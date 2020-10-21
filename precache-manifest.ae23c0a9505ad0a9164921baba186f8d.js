1c5c109b4be508b98ff72ae6bf6b8e7a082013cd
Choang9 <choang@seattleu.edu>
2020-10-20 21:45:03 -0700

    Updates

---
+++ precache-manifest.ae23c0a9505ad0a9164921baba186f8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ed9475446d7328c3f17ff5a44cd9da",
+    "url": "/Soccer-App/index.html"
+  },
+  {
+    "revision": "9a765a09a5bf8f3a9ea7",
+    "url": "/Soccer-App/static/css/main.efd32324.chunk.css"
+  },
+  {
+    "revision": "fd9f11b2912e075ad921",
+    "url": "/Soccer-App/static/js/2.289d1c9a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Soccer-App/static/js/2.289d1c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a765a09a5bf8f3a9ea7",
+    "url": "/Soccer-App/static/js/main.b21c2153.chunk.js"
+  },
+  {
+    "revision": "633536f4c6a257a723af",
+    "url": "/Soccer-App/static/js/runtime-main.b6c84283.js"
+  }
+]);
